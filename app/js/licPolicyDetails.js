5df6f874fa6150abbb2445fe7f2cf6abf722a56a
VALUE CREATORS <shrotshankar@valueCreators-MacBook-Air.local>
2014-11-25 07:46:53 +0530

    first and initial commit

---
+++ app/js/licPolicyDetails.js
@@ -0,0 +1,71 @@
+var policyDetail = {
+    "licPolicy": {
+        "--select--" : "select",
+        "Amulya Jeevan -2" : {
+             "d1" : "LIC Amulya Jeevan (Table - 823) is a term life insurance plan which provides financial protection to your family in case of unfortunate demise. Its similar to Anmol Jeevan - 2 with a minimum sum assured of 25 Lakhs. Sum assured lesser than 25 Lakhs can be taken in Anmol Jeevan. ",
+             "d2" : "You get good Sum Assured at very low priced premium. In the event of policyholder’s demise, the cover taken is paid to the nominee. There is no maturity benefit in LIC Amulya Jeevan- 2 as it’s a pure term life insurance plan. ",
+             "d3"  : "Service Tax is applicable on the premium paid for this policy, at the rate of 12.36% of the premium paid every year."     
+        },
+        "Anmol Jeevan - 2" : {
+            "d1" : "LIC Amulya Jeevan (Table - 823) is a term life insurance plan which provides financial protection to your family in case of unfortunate demise. Its similar to Anmol Jeevan - 2 with a minimum sum assured of 25 Lakhs. Sum assured lesser than 25 Lakhs can be taken in Anmol Jeevan.",
+            "d2" : "You get good Sum Assured at very low priced premium. In the event of policyholder’s demise, the cover taken is paid to the nominee. There is no maturity benefit in LIC Amulya Jeevan- 2 as it’s a pure term life insurance plan. ",
+            "d3" : "Service Tax is applicable on the premium paid for this policy, at the rate of 12.36% of the premium paid every year. "
+        },
+        "Jeevan Shagun" : {
+            "d1" : "LIC Jeevan Shagun (Table 826) is a Single Premium Money back policy with a duration of 12 years. Premium needs to be paid one time and Money back returns will be paid at the end of 10th and 11th policy year.",
+            "d2" : "15% of the Sum assured will be paid at the end of 10th year and 20% of the Sum assured will be paid at the end of 11th year. Remaining 65% Sum assured along with Loyalty addition (if any) will be paid at the end of 12th year.",
+            "d3" : "The insured person will be provided an insurance cover equal to 10 times the single premium paid under this policy. ",
+            "d4" : "Service Tax is applicable on the single premium paid, at the rate of 3.09% of the premium. "
+        },
+        "New BIMA BACHAT" : {
+            "d1" : "LIC New Bima Bachat Plan (Table 816) is a single premium Money Back Plan.In this plan the premium is paid only once. ",
+            "d2" : "15% of the Sum Assured is paid after every 3 years and the entire Single Premium paid plus Loyalty Additions is paid at the end of the policy term. ",
+            "d3" : "However, if the Life Insured dies within the policy tenure, the entire Sum Assured is paid irrespective of the amount of money paid as Survival Benefit. ",
+            "d4" : "Service Tax is applicable on the premium paid for this policy, at the rate of 3.09% of the lumpsum premium paid. "
+        },
+        "New Endowment Policy" : {
+            "d1" : "LIC New Endowment Policy (Table 814) is a pure investment plan with high bonus and liquidity facility incorporated. The Sum Assured along with accrued Bonus and Final Additional bonus (if any) will be paid at the end of the policy term. ",
+            "d2" : "This plan provides for disability benefit incorporated within it, where the future premiums would be waived off in case of permanent disability during the policy term. ",
+            "d3" : "An Accidental Death Benefit rider is available with payment of Rs.100/- per 1 Lakh sum assured per year. This is an optional rider, which if opted, twice the sum assured will be paid to the nominee in case of any accidental death.",
+            "d4" : "Service Tax is applicable on the premium paid for this policy, at the rate of 3.09% for the 1st year premium and 1.545% from 2nd year onwards. "
+        },
+        "New Jeevan Anand" : {
+            "d1" : "New Jeevan Anand (Table 815) provides insurance cover for entire life of the insured person. In this plan, Sum assured with accured bonus and Final Additional Bonus will be paid back at the end of the policy term. But the policy will continue till the death of the life insured or they complete 100 yrs of age, whichever occurs earlier. ",
+            "d2" : "If the insured person survives till his 100th age, a sum assured will be provided to him at the end of his 100th age. If the insured person expires before the completion of his 100th age, a Sum assured will be paid to the nominee. So it provides a risk coverage throughout the life of the insured person. ",
+            "d3" : "An Accidental Death Benefit rider is available with payment of Rs.100/- per 1 Lakh sum assured per year. This is an optional rider, which if opted, twice the sum assured will be paid to the nominee in case of any accidental death. ",
+            "d4" : "Service Tax is applicable on the premium paid for this policy, at the rate of 3.09% for the 1st year premium and 1.545% from 2nd year onwards. "   
+        },
+        "New Jeevan Nidhi" : {
+            "d1" : "LIC NEW JEEVAN NIDHI (Table 818) is a regulare premium pension plan. Premium needs to be paid till the end of the accumulation period. At the end of the accumulation period, the maturity comprising of the sum assured , accured bonus and the final additional bonus will be converted into pension. ",
+            "d2" : "After the end of the accumulation period, the vesting period starts in which pension will be paid out in regular intervals till the end of life for the life insured. ",
+            "d3" : "After the expiry of the life insured, a lumpsum will be paid to the nominee. ",
+            "d4" : "Service Tax is applicable on the premium paid for this policy, at the rate of 3.09% for the 1st year premium and 1.545% from 2nd year onwards. "
+        },
+        "New Money Back Policy" : {
+            "d1" : "LIC New Money Back Policy (Table 820 & 821) can be taken for a duration of 20 years or 25 years. A money back will be paid at the end of every 5 years. ",
+            "d2" : "In 20 years plan, 20 % of Sum assured will be paid at the end of 5 , 10 , 15 years. 40 % of Sum assured with the Accured Bonus will be paid at the end of 20th year. ",
+            "d3" : "In 25 years plan, 15 % of Sum assured will be paid at the end of 5 , 10 , 15 , 20 years. 40 % of Sum assured with the Accured Bonus will be paid at the end of 25 years.",
+            "d4" : "If the Life insured dies within the policy term, Sum Assured on Death ( 125% of SA or 10 times Annual Premium whichever is higher) with Bonus and Loyalty Additions (if any) will be paid to the nominee without detecting any of the money paid back earlier.",
+            "d5" : "Service Tax is applicable on the premium paid for this policy, at the rate of 3.09% for the 1st year premium and 1.545% from 2nd year onwards. "
+        },
+        "Single Premium Endowment Plan" : {
+            "d1" : "LIC Single Premium Endowment Plan (Table 817) is a single premium conventional With Profits Endowment Assurance Plan. In this policy, premium needs to be paid as a lumpsum at one time. At the end of the policy term, Sum assured with simple reversionary bonus and final additional bonus will be paid back.",
+            "d2" : "Life cover will be provided in this policy equal to the sum assured till the end of the policy term. ",
+            "d3" : "Service Tax is applicable on the premium paid for this policy, at the rate of 3.09% of the lumpsum premium paid. "
+        }
+    }
+};
+
+var policyYear = {
+    "Years" : {
+        "Single Premium Endowment Plan" : [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
+        "New Money Back Policy" : [20,25],
+        "New Jeevan Nidhi" : [7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35],
+        "New Jeevan Anand" : [15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35],
+        "New Endowment Policy" : [12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35],
+        "New BIMA BACHAT" : [9,12,15],
+        "Jeevan Shagun" : [12],
+        "Anmol Jeevan - 2" : [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
+        "Amulya Jeevan -2" : [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35]         
+    }
+};
